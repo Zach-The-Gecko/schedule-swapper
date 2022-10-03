7872d34cad9ed2c158b43728a7386239dd438f58
Zach G <Zach.the.gecko@gmail.com>
2022-10-02 21:28:10 -0600

    commit

--- src/Components/ListOfUsers/ListOfUsers.js
+++ src/Components/ListOfUsers/ListOfUsers.js
@@ -1,3 +1,5 @@
+// RN IT RETURNS THE INDEXES, STILL NEED TOP
+//MAP OVER LETTERS AND HIGHLIGHT SPECIFIC LETTERS SOMEHOW
 import React from "react";
 import { useState } from "react";
 import { useContext } from "react";
@@ -12,6 +14,29 @@ const ListOfUsers = ({ users }) => {
   const searchBarChange = (e) => {
     setSearchInput(e.target.value);
   };
+  const search = (input, displayName) => {
+    const regexStr = Array.from(input.replace(" ", "").toLowerCase()).reduce(
+      (acc, letter) => {
+        return acc + `${letter}.*`;
+      },
+      ""
+    );
+    const testReg = new RegExp(regexStr, "gm");
+    if (testReg.test(displayName.toLowerCase())) {
+      const searchedIndexes = Array.from(displayName.toLowerCase()).reduce(
+        (acc, letter, index) => {
+          if (input[acc[1]] === letter) {
+            return [[...acc[0], index], acc[1] + 1];
+          }
+          return acc;
+        },
+        [[], 0]
+      )[0];
+      return searchedIndexes;
+    }
+    return false;
+  };
+
   return (
     <div>
       <div className="SearchBoxUsers">
@@ -25,12 +50,10 @@ const ListOfUsers = ({ users }) => {
       <div className="ListOfUsers">
         {users
           .filter((name) => {
-            return name
-              ? name.displayName
-                  .toLowerCase()
-                  .replace(" ", "")
-                  .includes(searchInput.toLowerCase().replace(" ", ""))
-              : "";
+            if (name) {
+              return search(searchInput, name.displayName);
+            }
+            return false;
           })
           .map((user) => {
             if (user) {
